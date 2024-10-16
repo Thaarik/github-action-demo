1d7450628eaddb63511d5fd6ba54e1db11235cfc
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2024-10-16 10:29:44 +0100

    code

---
+++ src/test/java/com/ga/ga/demo/GaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.ga.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
